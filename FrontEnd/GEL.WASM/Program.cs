be4633a402bfc57cc563a756f25587dda91b485f
Huy Nguyen <huynguyen260398@gmail.com>
2021-10-18 20:50:03 +0700

    Added services in program.cs

--- FrontEnd/GEL.WASM/Program.cs
+++ FrontEnd/GEL.WASM/Program.cs
@@ -1,11 +1,19 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using GEL.WASM;
+using GEL.WASM.Services;
+using GEL.WASM.Services.IServices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+var config = builder.Configuration.Build();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddHttpClient<IPostService, PostService>();
+StaticDetails.ProductApiBase = config["ServiceUrls:PostApi"];
+builder.Services.AddScoped<IPostService, PostService>();
 
 await builder.Build().RunAsync();
