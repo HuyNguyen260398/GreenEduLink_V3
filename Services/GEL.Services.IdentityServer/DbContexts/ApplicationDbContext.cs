374b42b1b999a50e8f34d6e14818de949112a551
Huy Nguyen <huynguyen260398@gmail.com>
2021-10-23 17:15:10 +0700

    Added ApplicationDbContext, ApplicationUser and StaticDetails in IdentityServer project.

---
+++ Services/GEL.Services.IdentityServer/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using GEL.Services.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GEL.Services.IdentityServer.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Services/GEL.Services.IdentityServer/StaticDetails.cs
@@ -0,0 +1,39 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace GEL.Services.IdentityServer
+{
+    public static class StaticDetails
+    {
+        public const string Admin = "Admin";
+        public const string Viewer = "Viewer";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("GEL", "GEL Server"),
+                new ApiScope(name: "read", displayName: "Read your data."),
+                new ApiScope(name: "write", displayName: "Write your data."),
+                new ApiScope(name: "delete", displayName: "Delete your data."),
+            };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "read", "write", "profile" }
+                }
+            };
+    }
+}
